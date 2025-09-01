444d188d1cd7f74fd69976d72915cc987f259755
ptvthebest <brajkovic.mia5555@gmail.com>
2025-09-01 23:09:20 +0200

    Update casino.c

--- casino.c
+++ casino.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static const char* PLAYERS_FILENAME = "players.dat";
 static const char* GAMES_FILENAME = "games.dat";
@@ -51,10 +52,10 @@ void listPlayers() {
     printf("\nLista svih igrača:\n");
     for (int i = 0; i < playerCount; i++) {
         printf("%d. %s %s - Saldo: %.2lf\n",
-               players[i].playerID,
-               players[i].firstName,
-               players[i].lastName,
-               players[i].balance);
+            players[i].playerID,
+            players[i].firstName,
+            players[i].lastName,
+            players[i].balance);
     }
 }
 
@@ -63,14 +64,15 @@ void updatePlayer() {
     printf("Unesite ID igraca za azuriranje: ");
     (void)scanf("%d", &id);
 
-    for (int i = 0; i < playerCount; i++) {
-        if (players[i].playerID == id) {
-            printf("Unesite novi saldo: ");
-            (void)scanf("%lf", &players[i].balance);
-            savePlayers();
-            printf("Igrac azuriran.\n");
-            return;
-        }
+    qsort(players, playerCount, sizeof(Player), comparePlayerByID);
+
+    Player* p = findPlayerByID(id);
+    if (p) {
+        printf("Unesite novi saldo: ");
+        (void)scanf("%lf", &p->balance);
+        savePlayers();
+        printf("Igrac azuriran.\n");
+        return;
     }
     printf("Igrac s tim ID-om nije pronaden.\n");
 }
@@ -80,16 +82,18 @@ void deletePlayer() {
     printf("Unesite ID igraca za brisanje: ");
     (void)scanf("%d", &id);
 
-    for (int i = 0; i < playerCount; i++) {
-        if (players[i].playerID == id) {
-            for (int j = i; j < playerCount - 1; j++) {
-                players[j] = players[j + 1];
-            }
-            playerCount--;
-            savePlayers();
-            printf("Igrac obrisan.\n");
-            return;
+    qsort(players, playerCount, sizeof(Player), comparePlayerByID);
+
+    Player* p = findPlayerByID(id);
+    if (p) {
+        int idx = (int)(p - players);
+        for (int j = idx; j < playerCount - 1; j++) {
+            players[j] = players[j + 1];
         }
+        playerCount--;
+        savePlayers();
+        printf("Igrac obrisan.\n");
+        return;
     }
     printf("Igrac s tim ID-om nije pronaden.\n");
 }
@@ -135,199 +139,25 @@ void loadPlayers() {
     logOperation("Ucitani su podaci o igracima iz datoteke.");
 }
 
-int compareByBalance(const void* a, const void* b) {
-    double balanceA = ((Player*)a)->balance;
-    double balanceB = ((Player*)b)->balance;
-    return (balanceA > balanceB) - (balanceA < balanceB);
+int comparePlayerByID(const void* a, const void* b) {
+    int idA = ((const Player*)a)->playerID;
+    int idB = ((const Player*)b)->playerID;
+    return idA - idB;
 }
 
-void sortPlayers() {
-    qsort(players, playerCount, sizeof(Player), compareByBalance);
-    printf("Igraci sortirani po saldu.\n");
-    listPlayers();
-}
-
-Player* searchPlayerBinary(int id) {
-    int low = 0, high = playerCount - 1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (players[mid].playerID == id)
-            return &players[mid];
-        else if (players[mid].playerID < id)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return NULL;
+Player* findPlayerByID(int id) {
+    Player key;
+    key.playerID = id;
+    return (Player*)bsearch(&key, players, playerCount, sizeof(Player), comparePlayerByID);
 }
 
 void recursivePrintPlayers(int index) {
     if (index >= playerCount)
         return;
     printf("%d. %s %s - Saldo: %.2lf\n",
-           players[index].playerID,
-           players[index].firstName,
-           players[index].lastName,
-           players[index].balance);
+        players[index].playerID,
+        players[index].firstName,
+        players[index].lastName,
+        players[index].balance);
     recursivePrintPlayers(index + 1);
-}
-
-Player* findPlayerByID(int id) {
-    return searchPlayerBinary(id);
-}
-
-void addGame() {
-    Game* tmp = realloc(games, (gameCount + 1) * sizeof(Game));
-    if (!tmp) {
-        perror("Neuspjesno alociranje memorije za igre");
-        exit(EXIT_FAILURE);
-    }
-    games = tmp;
-
-    Game* g = &games[gameCount];
-    g->gameID = nextGameID++;
-    printf("Unesite ime igre: ");
-    (void)scanf("%49s", g->gameName);
-    printf("Unesite minimalnu okladu: ");
-    (void)scanf("%lf", &g->minBet);
-    printf("Unesite maksimalnu okladu: ");
-    (void)scanf("%lf", &g->maxBet);
-
-    gameCount++;
-    saveGames();
-    printf("Igra dodana: %s (ID: %d)\n", g->gameName, g->gameID);
-}
-
-void listGames() {
-    printf("\nLista svih igara:\n");
-    for (int i = 0; i < gameCount; i++) {
-        printf("%d. %s - Min: %.2lf, Max: %.2lf\n",
-               games[i].gameID,
-               games[i].gameName,
-               games[i].minBet,
-               games[i].maxBet);
-    }
-}
-
-void updateGame() {
-    int id;
-    printf("Unesite ID igre za azuriranje: ");
-    (void)scanf("%d", &id);
-    for (int i = 0; i < gameCount; i++) {
-        if (games[i].gameID == id) {
-            printf("Unesite novo ime igre (trenutno: %s): ", games[i].gameName);
-            (void)scanf("%49s", games[i].gameName);
-            printf("Unesite novi minimalni ulog (trenutno: %.2lf): ", games[i].minBet);
-            (void)scanf("%lf", &games[i].minBet);
-            printf("Unesite novi maksimalni ulog (trenutno: %.2lf): ", games[i].maxBet);
-            (void)scanf("%lf", &games[i].maxBet);
-            saveGames();
-            printf("Igra azurirana.\n");
-            return;
-        }
-    }
-    printf("Igra s tim ID-om nije pronadena.\n");
-}
-
-void deleteGame() {
-    int id;
-    printf("Unesite ID igre za brisanje: ");
-    (void)scanf("%d", &id);
-    for (int i = 0; i < gameCount; i++) {
-        if (games[i].gameID == id) {
-            for (int j = i; j < gameCount - 1; j++) {
-                games[j] = games[j + 1];
-            }
-            gameCount--;
-            saveGames();
-            printf("Igra obrisana.\n");
-            return;
-        }
-    }
-    printf("Igra s tim ID-om nije pronadena.\n");
-}
-
-void saveGames() {
-    FILE* file = fopen(GAMES_FILENAME, "wb");
-    if (!file) {
-        perror("Greska pri zapisu igara");
-        return;
-    }
-    fwrite(&gameCount, sizeof(int), 1, file);
-    fwrite(games, sizeof(Game), gameCount, file);
-    fclose(file);
-}
-
-void loadGames() {
-    FILE* file = fopen(GAMES_FILENAME, "rb");
-    if (!file) return;
-
-    if (fseek(file, 0, SEEK_END) != 0) {
-        perror("Greska prilikom pomicanja na kraj datoteke za igre");
-        fclose(file);
-        return;
-    }
-    long fileSize = ftell(file);
-    if (fileSize == -1L) {
-        perror("Greska prilikom citanja pozicije u datoteci za igre");
-        fclose(file);
-        return;
-    }
-    rewind(file);
-
-    fread(&gameCount, sizeof(int), 1, file);
-    games = malloc(gameCount * sizeof(Game));
-    if (!games && gameCount > 0) {
-        perror("Neuspjesno ucitavanje memorije za igre");
-        fclose(file);
-        exit(EXIT_FAILURE);
-    }
-    fread(games, sizeof(Game), gameCount, file);
-    nextGameID = gameCount > 0 ? games[gameCount - 1].gameID + 1 : 1;
-    fclose(file);
-    logOperation("Ucitane su igre iz datoteke.");
-}
-
-void sortGames() {
-    int choice;
-    printf("Sortirati igre po:\n1. Naziv\n2. Minimalni ulog\n3. Maksimalni ulog\nOdabir: ");
-    (void)scanf("%d", &choice);
-
-    for (int i = 0; i < gameCount - 1; i++) {
-        for (int j = 0; j < gameCount - i - 1; j++) {
-            int swap = 0;
-            if ((choice == 1 && strcmp(games[j].gameName, games[j + 1].gameName) > 0) ||
-                (choice == 2 && games[j].minBet > games[j + 1].minBet) ||
-                (choice == 3 && games[j].maxBet > games[j + 1].maxBet))
-                swap = 1;
-            if (swap) {
-                Game temp = games[j];
-                games[j] = games[j + 1];
-                games[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Igre sortirane.\n");
-    listGames();
-}
-
-void renameFile(const char* oldName, const char* newName) {
-    if (rename(oldName, newName) != 0)
-        perror("Greska pri preimenovanju datoteke");
-    else
-        printf("Datoteka je preimenovana.\n");
-}
-
-void deleteFile(const char* filename) {
-    if (remove(filename) != 0)
-        perror("Greska pri brisanju datoteke");
-    else
-        printf("Datoteka obrisana.\n");
-}
-
-void sortPlayersWithFunctionPointer(CompareFunc cmp) {
-    qsort(players, playerCount, sizeof(Player), cmp);
-    printf("Igraci sortirani (funkcijskim pokazivacem):\n");
-    listPlayers();
 }
